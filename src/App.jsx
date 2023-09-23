d3bcee1974bef0eb07d636a148b00e616fd80086
HackNetAyush <swami.ayushing1@gmail.com>
2023-09-23 14:54:00 +0530

    New Content

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* <h1 className='bg-red-500'>Hello</h1> */}
+
+      <div className='root w-full h-full flex items-center justify-center p-3'>
+        {/* <div className='main w-full h-full bg-[#000000e3] rounded-3xl flex items-center justify-center text-white'>
+          <div className="h"></div>
+        </div> */}
+      </div>
+      
+    </>
+  )
+}
+
+export default App
