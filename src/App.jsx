89bdac61caf108e2e1bbe9264b292f8f63d84a97
HackNetAyush <swami.ayushing1@gmail.com>
2023-09-27 00:07:26 +0530

    New Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import Login from './components/Login/Login.jsx'
 import './App.css'
 
 function App() {
@@ -8,13 +9,9 @@ function App() {
   return (
     <>
       {/* <h1 className='bg-red-500'>Hello</h1> */}
-
-      <div className='root w-full h-full flex items-center justify-center p-3'>
-        {/* <div className='main w-full h-full bg-[#000000e3] rounded-3xl flex items-center justify-center text-white'>
-          <div className="h"></div>
-        </div> */}
+      <div className='root w-full h-full flex items-center justify-center'>
+        <Login/>
       </div>
-      
     </>
   )
 }
